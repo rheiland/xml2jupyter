149d9e59073b138ac3d5c1ed2ad0406810acea42
Randy Heiland <heiland@indiana.edu>
2019-02-07 21:29:06 -0500

    flag for description as green buttons

--- xml2jupyter.py
+++ xml2jupyter.py
@@ -48,7 +48,7 @@ user_tab_header = """
 # Edit at your own risk.
 #
 import os
-from ipywidgets import Label,Text,Checkbox,Button,HBox,VBox,FloatText,IntText,BoundedIntText,BoundedFloatText
+from ipywidgets import Label,Text,Checkbox,Button,HBox,VBox,FloatText,IntText,BoundedIntText,BoundedFloatText,Layout,Box
     
 class UserTab(object):
 
@@ -112,10 +112,18 @@ indent2 = "          "
 widgets = {"double":"FloatText", "int":"IntText", "bool":"Checkbox", "string":"Text"}
 type_cast = {"double":"float", "int":"int", "bool":"bool", "string":""}
 vbox_str = "\n" + indent + "self.tab = VBox([\n"
+#param_desc_buttons_str = "\n" 
+desc_buttons_str = "\n" 
+row_str = "\n"
+box_str = "\n" + indent + "box_layout = Layout(display='flex', flex_flow='row', align_items='stretch', width='90%')\n"
+#        box1 = Box(children=row1, layout=box_layout)\n"
 
 # TODO: cast attributes to lower case before doing equality tests; perform more testing!
 
+param_desc_count = 0
 italicize_flag = False
+desc_as_button_flag = True
+desc_as_button_flag = False
 tag_list = []
 for child in uep:
     print(child.tag, child.attrib)
@@ -135,8 +143,20 @@ for child in uep:
         if italicize_flag:
             describe_str = child.attrib['description'] 
             describe_str = describe_str.replace(" ","\ ")
+        elif desc_as_button_flag:
+            describe_str = child.attrib['description']
+            param_desc_count += 1
+            # param_desc_button_name = "param_button" + str(param_desc_count)
+            # # param_desc_buttons_str += indent + param_desc_button_name + " = " + "Button(description='" + describe_str + "', disabled=True, layout={'width':'550px'}) \n"
+            # param_desc_buttons_str += indent + param_desc_button_name + " = " + "Button(description='" + describe_str + "', disabled=True, layout=param_button_layout) \n"
+            # param_desc_buttons_str += indent + param_desc_button_name + ".style.button_color = 'lightgreen' \n"
+            desc_row_name = "desc_row" + str(param_desc_count)
+            # desc_buttons_str += indent + desc_row_name + "[ \n"
+            desc_buttons_str += indent + desc_row_name + " = " + "Button(description='" + describe_str + "', disabled=True, layout=param_button_layout) \n"
+            desc_buttons_str += indent + desc_row_name + ".style.button_color = 'lightgreen' \n"
         else:
             describe_str = ' (' + child.attrib['description'] + ')'
+
     if 'units' in child.attrib.keys():
         if child.attrib['units'] != "dimensionless" and child.attrib['units'] != "none":
             units_str = child.attrib['units']
@@ -198,6 +218,13 @@ for child in uep:
                     vbox_str += indent2 + "HBox([" + full_name + ", Label('" + units_str + "'), " + " Label(r'\((" + describe_str + "\))')]), \n"
                 else:
                     vbox_str += indent2 + "HBox([" + full_name + ", Label('" + units_str + "'), ]), \n"
+            elif desc_as_button_flag:
+                row_name = "row" + str(param_desc_count)
+                row_str += indent +  row_name + " = [" + full_name + ", Label('" + units_str + "'), " + desc_row_name + "] \n"
+                box_name = "box" + str(param_desc_count)
+                box_str += indent + box_name + " = Box(children=" + row_name + ", layout=box_layout)\n"
+#        box1 = Box(children=row1, layout=box_layout)\n"
+                vbox_str += indent2 + box_name + ",\n"
             else:
                 vbox_str += indent2 + "HBox([" + full_name + ", Label('" + units_str + describe_str + "')]), \n"
 
@@ -223,6 +250,11 @@ print("run the Jupyter menu item:  Kernel -> Restart & Run All)")
 print()
 fp= open(user_tab_file, 'w')
 fp.write(user_tab_header)
+fp.write("\n" + indent + "param_button_layout={'width':'550px'} \n")
+#fp.write(param_desc_buttons_str)
+fp.write(desc_buttons_str)
+fp.write(row_str)
+fp.write(box_str)
 fp.write(vbox_str)
 fp.write(fill_gui_str)
 fp.write(fill_xml_str)
