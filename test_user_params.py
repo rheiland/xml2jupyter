57546d2c768c3db26d716acf91c864c30fc50d6d
Randy Heiland <heiland@indiana.edu>
2019-08-23 10:21:32 -0400

    two tabs

--- test_user_params.py
+++ test_user_params.py
@@ -9,17 +9,21 @@ import shutil
 import datetime
 from pathlib import Path
 from user_params import UserTab
+from microenv_params import MicroenvTab
 
 constWidth = '180px'
 tab_height = '500px'
+tab_height = '700px'
 tab_layout = widgets.Layout(width='950px',   # border='2px solid black',
                             height=tab_height, overflow_y='scroll',)
 
 # create the tab, but don't display it yet
+microenv_tab = MicroenvTab()
 user_tab = UserTab()
 
 # read in/parse the .xml file used to generate the GUI widgets.
-main_xml_filename = 'config-biorobots.xml'
+#main_xml_filename = 'config-biorobots.xml'
+main_xml_filename = 'config.xml'
 full_xml_filename = os.path.abspath(main_xml_filename)
 print('full_xml_filename=',full_xml_filename)
 
@@ -32,6 +36,7 @@ def write_config_file(name):
 #    full_xml_filename = os.path.abspath(full_xml_filename)
     tree = ET.parse(full_xml_filename)  # this file cannot be overwritten; part of tool distro
     xml_root = tree.getroot()
+    microenv_tab.fill_xml(xml_root)
     user_tab.fill_xml(xml_root)
     tree.write(name)
 
@@ -40,6 +45,7 @@ def fill_gui_params(config_file):
     tree = ET.parse(config_file)
     xml_root = tree.getroot()
     user_tab.fill_gui(xml_root)
+    microenv_tab.fill_gui(xml_root)
 
 # Create a 'Write' button that will update the .xml file with the parameters in the GUI
 def write_button_cb(s):
@@ -54,8 +60,8 @@ write_button = widgets.Button(
 write_button.on_click(write_button_cb)
 
 # Finally, create a simple Jupyter GUI consisting of just the user parameters tab and the Write button.
-titles = ['User Params']
-tabs = widgets.Tab(children=[user_tab.tab],
+titles = ['Microenvironment','User Params']
+tabs = widgets.Tab(children=[microenv_tab.tab, user_tab.tab],
                    _titles={i: t for i, t in enumerate(titles)},
                    layout=tab_layout)
 
